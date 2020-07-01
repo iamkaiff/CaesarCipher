32c4c3e2759d4efeb9c1e459346fff5bb8d53af5
Mohd Kaif <60395455+iamkaiff@users.noreply.github.com>
2020-07-01 13:55:26 +0530

    Add files via upload

---
+++ Numbers/Enc.java
@@ -0,0 +1,32 @@
+// importing the package
+import java.util.*;
+// defining a class
+class Enc{
+   // now creating the main method
+    public static void main(String args[])
+    {
+     /* creating scanner class to take
+	   input from user by using util package
+	   and we take predefined object "System.in" */ 
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter your PlainText: ");
+        String message = scan.nextLine();
+        int length = message.length();
+        System.out.print("Enter the Key: ");
+        int key = scan.nextInt();
+        String numbers = "0123456789";
+        System.out.print("The encrypted Text is: ");
+        for(int i = 0 ;i < length ;i++)
+        {
+            for(int j = 0; j < 26 ;j++)
+            {
+               if(j < numbers.length() && message.charAt(i) == numbers.charAt(j))
+                {
+                    System.out.print(numbers.charAt((j+key) % 10)); 
+                }
+                  
+            }
+        }
+        System.out.println();
+    }
+}
